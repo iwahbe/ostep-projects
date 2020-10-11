cd3e190bffaa6753faf1bfd7e675bf206418b039
Ian Wahbe <ian@wahbe.com>
2020-10-11 18:39:06 +0200

    Fix type in make_test_file

--- concurrency-pzip/make_test_file.sh
+++ concurrency-pzip/make_test_file.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+f1=bigTest.txt
+f2=bigTest2.txt 
+
 echo "Taking tests/6.in and"
-cat ./tests/6.in ./tests/6.in > bigTest.txt
+cat ./tests/6.in ./tests/6.in > $f1
 echo "Doubling it"
-cat bigTest.txt bigTest.txt > bigtest2.txt
+cat $f1 $f1 > $f2
 echo "Doubling it"
-cat bigTest2.txt bigTest2.txt > bigtest.txt
+cat $f2 $f2 > $f1
 echo "Doubling it"
-cat bigTest.txt bigTest.txt > bigtest2.txt
+cat $f1 $f1 > $f2
 echo "Doubling it"
-cat bigTest2.txt bigTest2.txt > bigtest.txt
+cat $f2 $f2 > $f1
 echo "Doubling it"
-cat bigTest.txt bigTest.txt > bigtest2.txt
+cat $f1 $f1 > $f2
 echo "Doubling it"
-cat bigTest2.txt bigTest2.txt > bigtest.txt
+cat $f2 $f2 > $f1
 echo "Doubling it"
 
-rm bigTest2.txt 
+
+rm $f2 
 echo "Thats 2^7 times bigger"
